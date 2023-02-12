156db92a35c57c9e5125692edc5b9f53ee673e9c
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-12 18:54:06 -0300

    Creating file java Conta.java and class primeiraConta for add slado to conta

---
+++ src/Conta.java
@@ -0,0 +1,7 @@
+public class Conta{
+	double saldo;
+	double salario;
+	int agencia;
+	int numero;
+	String titular;
+}
---
+++ src/CriaConta.java
@@ -0,0 +1,7 @@
+public class CriaConta {
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 4000;
+		System.out.println(primeiraConta.saldo);
+	}
+}

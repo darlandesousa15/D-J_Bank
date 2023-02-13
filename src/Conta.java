bf4b4f073842f4581ebfd340011c9d9b4ad41bad
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-13 04:53:20 -0300

    Add saldo for CriarCont and add class TesteReferencia

--- src/Conta.java
+++ src/Conta.java
@@ -4,4 +4,6 @@ public class Conta{
 	int agencia;
 	int numero;
 	String titular;
+	// Atributo são caracteristicas que especificam uma classe 
+	// Os atributos ou campos propiedades são as caracteristicas de um objeto, tudo aqui dentro são atributos
 }
--- src/CriaConta.java
+++ src/CriaConta.java
@@ -1,7 +1,17 @@
 public class CriaConta {
 	public static void main(String[] args) {
+		
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 4000;
 		System.out.println(primeiraConta.saldo);
+		System.out.println("Saldo da 1ª conta " + primeiraConta.saldo);
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 2000;
+		System.out.println(segundaConta.saldo);
+		System.out.println("Saldo da 2ª conta " + segundaConta.saldo);
+		
 	}
 }

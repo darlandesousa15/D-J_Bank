c76a68af23af7661007a2ec626277bc8392aa4ac
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-13 05:26:19 -0300

    Comparing object Conta with outher object Conta

--- src/TesteReferencia.java
+++ src/TesteReferencia.java
@@ -1,4 +1,19 @@
 
 public class TesteReferencia {
 
+	public static void main(String[] args) {
+		
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 500;
+		System.out.println("Saldo é de: " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta;
+		segundaConta.saldo += 100;
+		System.out.println("Saldo é de: " + segundaConta.saldo);
+		
+		if(primeiraConta == segundaConta) {
+			System.out.println("São as mesmas contas");
+		}
+	}
+	
 }

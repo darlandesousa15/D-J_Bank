25e785294161ef68cce90f24aa3cd30c05070b2e
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-17 04:52:26 -0300

    Change files

--- src/Conta.java
+++ src/Conta.java
@@ -1,16 +1,23 @@
 public class Conta {
 
 	private double saldo;
-	private double salario;
 	private int agencia;
 	private int numero;
-	Cliente titular;
+	private Cliente titular;
+	private static int total;
+	// static é para indicar que é da classe não do objeto, stati é um atributo da classe
 	// Atributo são caracteristicas que especificam uma classe
 	// Os atributos ou campos propiedades são as caracteristicas de um objeto, tudo
 	// aqui dentro são atributos
 
-	public void salario() {
-		salario = 500;
+	public Conta (int agencia, int numero) {
+		Conta.total++;
+		this.agencia = agencia;
+		this.numero = numero;
+		if (numero <= 0 || agencia <= 0) {
+			System.out.println("O número da conta e agencia não pode ser negativa. Altere para positivos");
+		}
+		System.out.println("Criando conta");
 	}
 	
 	public void deposita(double valor) {
@@ -24,16 +31,8 @@ public class Conta {
 		} else {
 			return false;
 		}
-	}
-	
-	public double getSalario() {
-		return this.salario;
-	}
-	
-	public void setSalario(double salario) {
-		this.salario = salario;
-	}
 
+	}
 
 	public boolean transfere(double valor, Conta destino) {
 		if (this.saldo >= valor) {
@@ -43,31 +42,46 @@ public class Conta {
 		}
 		return false;
 	}
-	
-	//Isso é um methodo e que ao ser envocado obigatoriamente tem que usar os ()
+
+	// Isso é um methodo e que ao ser envocado obigatoriamente tem que usar os ()
 	public double getSaldo() {
 		return this.saldo;
 	}
-	
-	public int getAgencia() {
-		return agencia;
+
+	public int getNumero() {
+		return this.numero;
 	}
-	
+
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("O número da conta não pode ser negativa.");
+			return;
+		}
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
 	public void setAgencia(int agencia) {
-		if (agencia < 0) {
-			System.out.println("A Agencia tem que ser positiva!");
+		if (numero <= 0) {
+			System.out.println("A Agêcia não pode ser negativa.");
 			return;
 		}
 		this.agencia = agencia;
 	}
-	public int getNumero() {
-		return numero;
+
+	public Cliente getTitular() {
+		return titular;
 	}
-	public void setNumero(int numero) {
-		if (numero < 0) {
-			System.out.println("O numero da conta tem que ser positiva!");
-		}
-		this.numero = numero;
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
 	}
 	
+	public static int getTotal() {
+		return Conta.total;
+	}
+
 }

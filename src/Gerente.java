9c94e8a6b586a922267bf4b2959ed7f427056e48
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-23 05:15:48 -0300

    resolving some coments in class

--- src/Gerente.java
+++ src/Gerente.java
@@ -10,11 +10,12 @@ public class Gerente extends Funcionario implements Autenticavel {
 	public double getBonificacao() {
 		System.out.println("Chamando o metodo do gerente");
 		return getSalario();
-		// O super esta substituindo o this nesse metodo pq o salario e o getbonificação
-		// estão na classe mãe
-		// e o super faz olhar para a classe mãe onde foi armazenado esses dados, por
-		// isso estamos usando o super ñ o this aqui.
-		// obrigado a utilizar o super pois os dados estão definidos na classe mãe
+		/*
+		 * O super esta substituindo o this nesse metodo pq o salario e o getbonificação
+		 * estão na classe mãe e o super faz olhar para a classe mãe onde foi armazenado
+		 * esses dados, por isso estamos usando o super ñ o this aqui. obrigado a
+		 * utilizar o super pois os dados estão definidos na classe mãe
+		 */
 	}
 
 	public void setSenha(int senha) {

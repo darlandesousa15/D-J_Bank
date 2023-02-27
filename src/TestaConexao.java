ed7c250455a2b9301bd37091d208d5d2f391b705
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-27 13:34:33 -0300

    add exception for conection

--- src/TestaConexao.java
+++ src/TestaConexao.java
@@ -2,20 +2,21 @@
 public class TestaConexao {
 
 	public static void main(String[] args) {
-		
-		Conexao con = null;
 
-		try {
-			con = new Conexao();
-			con.leDados();
-			con.fecha();
-		} catch (Exception ex) {
-			System.out.println("Deu erro de conexão");
-
-		} finally {
-			// esse bloco finally sempre será executado independente do que aconteça
-			con.fecha();
+		try (Conexao conexao = new Conexao()) {
+			conexao.leDados();
+		} catch (Exception ex){
+			System.out.println("Deu erro");
 		}
+		/*
+		 * Conexao con = null;
+		 * 
+		 * try { con = new Conexao(); con.leDados(); con.fecha(); } catch (Exception ex)
+		 * { System.out.println("Deu erro de conexão");
+		 * 
+		 * } finally { // esse bloco finally sempre será executado independente do que
+		 * aconteça con.fecha(); }
+		 */
 
 	}
 

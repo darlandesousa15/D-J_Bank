9238a82fd9c8e762c7a254c3b102adcda5fdce18
darlandesousa15 <darlandesousa15@gmail.com>
2023-03-01 12:57:55 -0300

    creating array of clients

--- src/TestandoListas.java
+++ src/TestandoListas.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
 
 public class TestandoListas {
 
 	public static void main(String[] args) {
+		
+		String cliente1 = "Adicionando o primeiro cliente";
+		String cliente2 = "Cliente 2 é aquele cara das antigas";
+		String cliente3 = "Adicionando aquele cliente camarada";
+		
+		ArrayList<String> cliente = new ArrayList<>();
+		//cria a array list capturando a String, mas só adicionando as que foram adicionadas abaixo
+		cliente.add(cliente1);
+		cliente.add(cliente2);
+		cliente.add(cliente3);
 		
 		
 		

c77c33f71a0de6160c0ccc5d6edcbd06973e1166
darlandesousa15 <darlandesousa15@gmail.com>
2023-03-03 13:17:34 -0300

    altering for of testandolista

--- src/TestandoListas.java
+++ src/TestandoListas.java
@@ -4,21 +4,27 @@ import java.util.Collections;
 public class TestandoListas {
 
 	public static void main(String[] args) {
-		
+
 		String cliente1 = "Adicionando o primeiro cliente";
 		String cliente2 = "Cliente 2 é aquele cara das antigas";
 		String cliente3 = "Adicionando aquele cliente camarada";
-		
+
 		ArrayList<String> cliente = new ArrayList<>();
-		//cria a array list capturando a String, mas só adicionando as que foram adicionadas abaixo
+		// cria a array list capturando a String, mas só adicionando as que foram
+		// adicionadas abaixo
 		cliente.add(cliente1);
 		cliente.add(cliente2);
 		cliente.add(cliente3);
+
 		
+		 //for (int i = 0; i<= cliente.size(); i++) { System.out.println("Cliente: " +
+		 //cliente.get(i)); }
+		 
+
 		System.out.println(cliente + " Antes");
 		Collections.sort(cliente);
 		System.out.println(cliente + " Depois");
-		
+
 	}
-	
+
 }

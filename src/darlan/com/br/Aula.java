113a2ff8b4d1a590cc51914dbae5c8b9c34ba64a
darlandesousa15 <darlandesousa15@gmail.com>
2023-03-08 07:02:41 -0300

    resolving problem with class aula

--- src/darlan/com/br/Aula.java
+++ src/darlan/com/br/Aula.java
@@ -2,18 +2,31 @@ package darlan.com.br;
 
 import java.util.ArrayList;
 
-public class Aula {
-	
-	private String curso;
-	private int tempo;
-	private ArrayList<>;
+public class Aula implements Comparable<Aula> {
 
-	public Aula(String curso, int tempo) {
-		this.curso = curso;
+	private String titulo;
+	private int tempo;
+
+	public Aula(String titulo, int tempo) {
+		this.titulo = titulo;
 		this.tempo = tempo;
-		
 	}
 	
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public int getTempo() {
+		return tempo;
+	}
+
+	public String toString() {
+		return "[Aula: " + this.titulo + ", " + this.tempo + "minutos]";
+	}
+	@Override
+	public int compareTo(Aula o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
-
-

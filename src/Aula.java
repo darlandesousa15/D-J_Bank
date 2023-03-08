a2485667479b1989c7636cc6b068ced0c478a8c6
darlandesousa15 <darlandesousa15@gmail.com>
2023-03-07 23:15:06 -0300

    add class and creating cursos for use in testacurso

---
+++ src/Aula.java
@@ -0,0 +1,15 @@
+
+public class Aula {
+	
+	String curso;
+	int tempo;
+
+	public Aula(String curso, int tempo) {
+		this.curso = curso;
+		this.tempo = tempo;
+		
+	}
+	
+}
+
+
---
+++ src/darlan/com/br/TestaCurso.java
@@ -0,0 +1,20 @@
+package darlan.com.br;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestaCurso {
+
+	public static void main(String[] args) {
+		
+		String curso = "Definindo funções";
+		String curso1 = "Resolução de problemas orientado ao objeto";
+		String curso2 = "Design Thinking";
+		
+		ArrayList<String> curso = new ArrayList<>();
+		
+		
+
+	}
+
+}

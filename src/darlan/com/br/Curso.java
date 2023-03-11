b8bd647a364c5d1c1c4e56c63934e9b25ffad94d
darlandesousa15 <darlandesousa15@gmail.com>
2023-03-11 17:53:31 -0300

    Creating methodo to add aluno in list hashset

--- src/darlan/com/br/Curso.java
+++ src/darlan/com/br/Curso.java
@@ -1,14 +1,17 @@
 package darlan.com.br;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<aluno> alunos = new HashSet<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -39,6 +42,11 @@ public class Curso {
 	public String toString() {
 		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal()
 				+ ", aulas: + " + this.aulas + "]";
+	}
+
+	public void matricula(aluno aluno) {
+		this.alunos.add(aluno);
+		
 	}
 
 }
--- src/darlan/com/br/TestaCursoComAluno.java
+++ src/darlan/com/br/TestaCursoComAluno.java
@@ -2,4 +2,26 @@ package darlan.com.br;
 
 public class TestaCursoComAluno {
 
+	public static void main(String[] args) {
+		
+		Curso javaColecoes = new Curso("Dominando as coleções do Java",
+				"Paulo Silveira");
+
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
+		
+		aluno a1 = new aluno("João Mariano", 589741);
+		aluno a2 = new aluno("Eloá da Silva", 829453);
+		aluno a3 = new aluno("Mauricio Auves", 528467);
+		
+		javaColecoes.matricula(a1);
+		javaColecoes.matricula(a2);
+		javaColecoes.matricula(a3);
+		
+		
+		
+		System.out.println(a1.getNome());
+	}
+	
 }

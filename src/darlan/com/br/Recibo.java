233342c2c12c78e881a4e3f68917daae4632e87e
darlandesousa15 <darlandesousa15@gmail.com>
2023-03-16 05:32:05 -0300

    add class and doing tests

---
+++ src/darlan/com/br/Recibo.java
@@ -0,0 +1,20 @@
+package darlan.com.br;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Recibo {
+
+	//private Set<Recibo> Recibo = new TreeSet<>();
+
+
+Recibo rec1 = new Recibo();
+
+Recibo rec2 = new Recibo();
+// atribui valores
+Recibo rec3 = new Recibo();
+// atribui valores
+
+rec1.add(rec1);
+
+}

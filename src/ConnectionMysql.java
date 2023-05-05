f19821b55108a66bcf3ff0b29b796f3d1ed2aa15
darlandesousa15 <darlandesousa15@gmail.com>
2023-05-05 06:18:31 -0300

    creating connection for database and using data

--- src/ConnectionMysql.java
+++ src/ConnectionMysql.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectionMysql {
 
@@ -9,6 +11,26 @@ public class ConnectionMysql {
 		Connection connection = DriverManager
 				.getConnection("jdbc:mysql://localhost/djbank?useTimezone=true&serverTimezone=UTC" , "root" , "9103-Darlan@");
 		
+		Statement stm = connection.createStatement();
+		stm.execute("SELECT * FROM tbcliente");
+		ResultSet rst = stm.getResultSet();
+		
+		while (rst.next()) {
+			String nome = rst.getString("NOME");
+			System.out.println(nome);
+			String cpf = rst.getString("CPF");
+			System.out.println(cpf);
+			String endereco = rst.getString("ENDERECO1");
+			System.out.println(endereco);
+			String nascimento = rst.getString("DATA_NASCIMENTO");
+			System.out.println(nascimento);
+			String profissao = rst.getString("PROFISSAO");
+			System.out.println(profissao);
+		}
+		
+		System.out.println("fechando conexão!");
+		
+		connection.close();
 	}
 
 }
